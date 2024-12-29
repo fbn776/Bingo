c991daa3d2074aef3de800031caccea91d4b1eb2
fbn776 <fbn776@gmail.com>
2024-12-29 20:58:22 +0530

    fix: current state data now starts from 1

--- backend/src/lib/data.ts
+++ backend/src/lib/data.ts
@@ -1,3 +1,3 @@
 import {TLocalBoardCell} from "../../../common/types";
 
-export const DEFAULT_BOARD_STATE: TLocalBoardCell[] = Array.from({length: 25}).map((_, i) => ({num: i, selected: false}));
+export const DEFAULT_BOARD_STATE: TLocalBoardCell[] = Array.from({length: 25}).map((_, i) => ({num: i + 1, selected: false}));
